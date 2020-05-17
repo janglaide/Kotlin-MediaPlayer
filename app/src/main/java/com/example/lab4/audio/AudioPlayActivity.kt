37333e48bac105f6ad38ff9b10c9f8b66776bb7f
Alisa Proshchenko <ajanglaide@gmail.com>
2020-05-17 16:37:59 +0300

    static audio with seekbar works

--- app/src/main/java/com/example/lab4/audio/AudioPlayActivity.kt
+++ app/src/main/java/com/example/lab4/audio/AudioPlayActivity.kt
@@ -1,18 +1,23 @@
 package com.example.lab4.audio
 
+import android.annotation.SuppressLint
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Message
 import android.view.View
 import android.widget.SeekBar
+import android.widget.Toast
 import com.example.lab4.R
+import kotlinx.android.synthetic.main.activity_audio_play.*
 
 
 class AudioPlayActivity() : AppCompatActivity() {
 
-    private val mPlayer : MediaPlayer = MediaPlayer() //MediaPlayer.create(this, R.raw.joy_division_love_will_tear_us_apart)
-
-    //private val seekBar : SeekBar = findViewById(R.id.audioSeekBar)
+    private var mPlayer : MediaPlayer = MediaPlayer()
+    private lateinit var runnable : Runnable
+    private var handler: Handler = Handler()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,32 +26,57 @@ class AudioPlayActivity() : AppCompatActivity() {
         val actionBar = supportActionBar
         actionBar!!.title = "Audios"
 
-        val mPlayer = MediaPlayer.create(this, R.raw.joy_division_love_will_tear_us_apart)
-        val tmp = mPlayer.duration
-        //seekBar.max = mPlayer.duration
-        /*seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
-            override fun onStopTrackingTouch(seekBar: SeekBar?) { }
+        mPlayer = MediaPlayer.create(this, R.raw.joy_division_love_will_tear_us_apart)
 
-            override fun onStartTrackingTouch(seekBar: SeekBar?) { }
+        mPlayer.start()
 
-            override fun onProgressChanged(
-                seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+        initializeSeekBar()
+
+        audioSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
                 if(fromUser){
-                    //mPlayer.seekTo(progress)
-                    seekBar?.progress = mPlayer.currentPosition
+                    mPlayer.seekTo(progress * 1000)
+
+                    Toast.makeText(
+                        this@AudioPlayActivity,
+                        (mPlayer.currentPosition / 1000 / 60).toString() + " min, " +
+                                (mPlayer.currentPosition / 1000 % 60).toString() + " sec",
+                        Toast.LENGTH_SHORT
+                    ).show()
                 }
             }
-        })*/
 
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+            }
+        })
+
+        }
+    private fun initializeSeekBar() {
+        audioSeekBar.max = mPlayer.duration / 1000
+
+        runnable = Runnable {
+            audioSeekBar.progress = mPlayer.currentPosition / 1000
+
+            val diff = mPlayer.duration / 1000 - mPlayer.currentPosition / 1000
+
+            handler.postDelayed(runnable, 1000)
+        }
+        handler.postDelayed(runnable, 1000)
     }
+
     fun onPlay(view: View){
-        //mPlayer.start()
+        mPlayer.start()
     }
     fun onPause(view: View){
-        //mPlayer.pause()
+        mPlayer.pause()
     }
     fun onStop(view: View){
-        //mPlayer.stop()
-        onDestroy()
+        mPlayer.stop()
+        finish()
     }
 }
